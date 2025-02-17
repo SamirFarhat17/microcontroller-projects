03ddaf3a773cfe69ad2d53e972081d3fce528624
Samir Farhat <samirfarhat17@gmail.com>
2025-02-16 20:17:24 -0500

    More basics w multimeter

--- blink.ino/blink.ino.ino
+++ blink.ino/blink.ino.ino
@@ -1,12 +1,19 @@
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(3,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN,HIGH);
-  delay(10);
+  delay(100);
   digitalWrite(LED_BUILTIN,LOW);
-  delay(500);
+  delay(100);
+  
+  digitalWrite(3,HIGH);
+  delay(100);
+  digitalWrite(3,LOW);
+  delay(100);
 }
